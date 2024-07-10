c24e5d6314a85bcf2b28c845e8295126ff41d040
Vikas Narwariya <122281547+vikas-narwariya831@users.noreply.github.com>
2024-07-10 20:26:08 +0530

    v2

--- src/component/FoodItem/FoodItem.jsx
+++ src/component/FoodItem/FoodItem.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { assets } from '../assets/assets';
 
 function FoodItem({ id, name, price, description, image }) {
   console.log(image);
+  const [itemCount,setItemCount]=useState(0)
   return (
     <div className='w-full m-auto transition duration-300 rounded-2xl shadow-custom-black animate-fadeIn'>
       <div className='w-full overflow-hidden rounded-t-2xl'>
         <img src={image} alt={name} className='object-cover w-full h-auto' />
+        {!itemCount
+        ?<img  onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt={assets.add_icon_green} />
+        :<div className='flex items-center gap-2 pl-2 mt-3'>
+                  <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+
+        <p className='font-semibold'>  {itemCount}</p>
+        <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
+
+
+        </div>
+          
+        }
       </div>
       <div className='p-4'>
         <div className='flex items-center justify-between mb-2'>
@@ -14,7 +27,7 @@ function FoodItem({ id, name, price, description, image }) {
           <img src={assets.rating_starts} alt="Rating" className='w-20' />
         </div>
         <p className='mb-2 text-gray-600'>{description}</p>
-        <p className='text-xl font-bold'>${price}</p>
+        <p className='text-xl font-bold text-tomato'>${price}</p>
       </div>
     </div>
   );
